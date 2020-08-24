a0782e1d4d28f5b79aaed08dc5185d9ec1bfc529
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-24 19:12:35 +0900

    写経＋アレンジ

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 package battle01;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    int playerHP = 5;
+    int computerHP = 8;
+
+    System.out.println("敵が現れた！");
+    System.out.println("");
+    System.out.println("(*ﾟ▽ﾟ*)");
+    System.out.println("");
+
+    do {
+      System.out.println("冒険者が敵に１のダメージ！");
+      computerHP--;
+      System.out.println("敵HP：" + computerHP);
+      if (computerHP != 0) {
+        System.out.println("敵が冒険者に１のダメージ！");
+        playerHP--;
+        System.out.println("冒険者HP：" + playerHP);
+        if (playerHP == 0) {
+          System.out.println("敵に倒された");
+        }
+      } else {
+        System.out.println("敵を倒した");
+      }
+    } while(playerHP > 0 && computerHP > 0);
+  }
 }
