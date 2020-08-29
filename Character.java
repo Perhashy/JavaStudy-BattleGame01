45cafbf0f4d12427a67746fc274c097ae404c5d3
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-29 22:07:24 +0900

    命中率用のフィールドと配列とコンストラクタ

--- Character.java
+++ Character.java
@@ -5,14 +5,17 @@ import java.util.Random;;
 public class Character {
   private int hp;
   private int attack;
+  private int hitRate;
 
   int[] arrayHp = {500, 1000, 2000};
   int[] arrayAttack = {100, 250, 500};
+  int[] arrayHit = {30, 50, 70, 90};
 
   public Character() {
     Random random = new Random();
     this.hp = arrayHp[random.nextInt(arrayHp.length)];
     this.attack = arrayAttack[random.nextInt(arrayAttack.length)];
+    this.hitRate = arrayHit[random.nextInt(arrayHit.length)];
   }
 
   public void setHp(int hp) {
